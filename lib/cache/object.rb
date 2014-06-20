83317efcde0e900bf7290ff0eaccb7738643adb1
Server Cimen & Siyamed Sinir <server+siyamed@wanelo.com>
2014-06-20 13:52:20 -0700

    first commit

---
+++ lib/cache/object.rb
@@ -0,0 +1,7 @@
+require "cache/object/version"
+
+module Cache
+  module Object
+    # Your code goes here...
+  end
+end
---
+++ lib/cache/object/version.rb
@@ -0,0 +1,5 @@
+module Cache
+  module Object
+    VERSION = "0.0.1"
+  end
+end
